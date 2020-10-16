dc0725bfc634969a06d1a6d39a46a9233a2973bc
Trantow <trantow@kreispb.gkd>
2020-10-16 09:54:17 +0200

    Anlegen eines SOAPUI-Test-Projektes

---
+++ config/environment.bat
@@ -0,0 +1,12 @@
+
+REM Zentrale Definition der Endungen von Log-Files
+@for /F "tokens=1,2" %%d in ('date /T') do set xdate=%%d
+@for /F "tokens=1,2" %%e in ('time /T') do set xtime=%%e
+SET ENV_LOGDATE=%xDATE:~6,4%-%xDATE:~3,2%-%xDATE:~0,2%
+SET ENV_LOGDATETIME=%xDATE:~6,4%-%xDATE:~3,2%-%xDATE:~0,2%_%xtime:~0,2%-%xtime:~3,2%
+
+REM Vorbelegung von Variablen mit dummy-Werten
+
+
+REM Überladen von Variablen durch spezifischen Festlegungen
+IF EXIST "%~dp0environment_custom.bat" CALL "%~dp0environment_custom.bat"
---
+++ tests/soapui/run_all_tests.bat
@@ -0,0 +1,40 @@
+@echo off
+
+REM Define general settings
+SET TEST_DIR=%~dp0
+SET BASE_DIR=%~dp0..\..\
+CALL %BASE_DIR%\config\environment.bat
+
+REM Relative dir for soap-ui Logs (used as well by junit-viewer)
+SET SOAP_LOG_DIR_NAME=logs\%ENV_LOGDATETIME%\
+
+REM Absoute path for soap-ui needed by Soap-UI itself
+SET MY_LOGDIR=%~dp0%SOAP_LOG_DIR_NAME%
+mkdir %MY_LOGDIR%
+
+REM RUN the SOAP-UI tests
+SET PROJECT_FILE="%~dp0qs-repo-check-soapui-project.xml"
+cd %MY_LOGDIR%
+call "C:\Programme portable\SoapUI-5.4.0-EB\bin\testrunner.bat" -PAGS_SERVER_TOKEN=%AGS_SERVER_TOKEN_SSL% -PAGS_SERVER_TOKEN_INTRA=%AGS_SERVER_TOKEN_INTRA% -r -j -f%MY_LOGDIR% %PROJECT_FILE% > %MY_LOGDIR%test_run.log
+cd %TEST_DIR%
+
+REM Create HTML out of soap-ui's junit results
+call junit-viewer --results=%SOAP_LOG_DIR_NAME% --save=%SOAP_LOG_DIR_NAME%results.html --minify=false  --contracted
+
+REM Find out if an error occurs
+set file=%SOAP_LOG_DIR_NAME%soapui-errors.log
+FOR /F "usebackq" %%A IN ('%file%') DO set size=%%~zA
+echo %size%
+set error_file="C:\Users\%USERNAME%\Desktop\ERROR_qs-repo-check.html"
+echo %error_file%
+
+REM if error is found copy HTML to Desktop
+if %size% LSS 1 (
+    REM all is fine
+) ELSE (
+	
+	IF NOT EXIST %error_file% (
+        echo dummy > %error_file%
+    )
+    xcopy "%SOAP_LOG_DIR_NAME%results.html" %error_file% /y /i
+)
